e59c66554f22779562af8a6846f413209be024d8
den.akifev <den.akifev@mail.ru>
2024-07-04 13:34:15 +0300

    fix

--- task2/solve.py
+++ task2/solve.py
@@ -1,6 +1,6 @@
 l1 = float(input())
 l2 = float(input())
 l3 = float(input())
-a = l1*l2 - l3 / 2.5 if l1 > l2 else 2
+a = l1*l2 - l3 / 2.5 if l1 > l2 else 2.0
 b = 15 - (abs(l1))**0.5 if l1 == l3 else l1 + 2*l2
-print(a, b)
+print(f"a = {a}\nb = {b}")
